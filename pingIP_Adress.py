67204850d29922b582dfcf4068919b79b523f3d9
xXM-DevelopmentXx <112732832+xXM-DevelopmentXx@users.noreply.github.com>
2022-09-05 20:35:44 +0200

    Add files via upload

---
+++ pingIP_Adress.py
@@ -0,0 +1,25 @@
+#Importiert das Modul OS
+import os
+
+#### Cleart die Console ####
+os.system("cls")
+
+#### Printet text etc in die Console ####
+print("#" *60)
+
+#### Eingabe der IP Adresse die gepingt werden soll ####
+IP_die_gepingt_werde_soll = input("Gebe hier Die IP-Adresse ein die Gepingt werden soll: ")
+
+#### Printet Text etc in die Console ####
+print("-" *60)
+#### OS soll die IP-Adresse pingen ####
+os.system("ping {}" .format(IP_die_gepingt_werde_soll))
+
+#### Printet text etc in die Console ####
+print("-" *60)
+
+################################# INFORMATION #################################
+print("Coded by mxrvin#5452 \nOwner von M-Development \nM-Development Discord: https://discord.gg/JUuxCYJA5W \nM-Development Github: https://github.com/xXM-DevelopmentXx")
+
+#### Wartet auf eingabe einer taste um das Programm zu schließen ####
+input("Drücke eine Taste um das Programm zu schließen!")
